35e643661980c83adb34b7803a13756d0ec9f0bc
Hirofumi Wakasugi <baenej@gmail.com>
2016-04-14 15:28:42 +0900

    Sorcery simple password authentication

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+  root to: 'users#index'
+  resources :sessions
+  resources :users
+
+  get 'login' => 'sessions#new', as: :login
+  post 'logout' => 'sessions#destroy', as: :logout
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   # Serve websocket cable requests in-process
