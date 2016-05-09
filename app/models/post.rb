5918c6197a10bb52dec0bc69b3292051266a3965
Hirofumi Wakasugi <baenej@gmail.com>
2016-05-09 10:18:55 +0900

    Add code syntax highlighting via rouge

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,6 +2,6 @@ class Post < ApplicationRecord
   belongs_to :user
 
   def body_html
-    Kramdown::Document.new(body, input: 'GFM').to_html
+    Kramdown::Document.new(body, input: 'GFM', syntax_highlighter: 'rouge').to_html
   end
 end
