3a4d97e27fe13624425d5d56e8f3019bd8fe5d8c
Hirofumi Wakasugi <baenej@gmail.com>
2016-05-09 12:31:30 +0900

    Add tests for Post#body_html

--- test/controllers/posts_controller_test.rb
+++ test/controllers/posts_controller_test.rb
@@ -2,7 +2,7 @@ require 'test_helper'
 
 class PostsControllerTest < ActionDispatch::IntegrationTest
   setup do
-    @post = posts(:one)
+    @post = posts(:alice_in_wonderland)
     @user = users(:alice)
   end
 
--- test/models/post_test.rb
+++ test/models/post_test.rb
@@ -1,7 +1,36 @@
 require 'test_helper'
 
 class PostTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'body_html should return rendered html' do
+    post = posts(:alice_in_wonderland)
+    expected = <<-EOS
+<h1 id="down-the-rabbit-hole">Down the Rabbit-Hole</h1>
+
+<h2 id="beginning">Beginning</h2>
+
+<p>Alice was beginning to get very tired of sitting by her sister on the bank,<br />
+and of having nothing to do.</p>
+
+<h2 id="considering">Considering</h2>
+
+<ul>
+  <li>so she was considering</li>
+  <li>in her own mind</li>
+</ul>
+    EOS
+    assert_equal(expected, post.body_html)
+  end
+
+  test 'fenced code block rendered as expected' do
+    post = posts(:fenced_code_block)
+    expected = <<-EOS
+<div class="language-ruby highlighter-rouge"><pre class="highlight"><code><span class="k">def</span> <span class="nf">foo</span>
+  <span class="nb">puts</span> <span class="s1">'foo'</span>
+<span class="k">end</span>
+</code></pre>
+</div>
+    EOS
+    puts post.body_html
+    assert_equal(expected, post.body_html)
+  end
 end
