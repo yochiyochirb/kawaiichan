5bf32db28c2579806eb35eac97fc414899e3e7c5
Hirofumi Wakasugi <baenej@gmail.com>
2016-05-16 12:25:53 +0900

    Remove authentications table

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,3 @@
 class User < ActiveRecord::Base
-  authenticates_with_sorcery! do |config|
-    config.authentications_class = Authentication
-  end
-
-  has_many :authentications, dependent: :destroy
   has_many :posts
-
-  accepts_nested_attributes_for :authentications
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,16 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160508030520) do
-
-  create_table "authentications", force: :cascade do |t|
-    t.integer  "user_id",    null: false
-    t.string   "provider",   null: false
-    t.string   "uid",        null: false
-    t.datetime "created_at"
-    t.datetime "updated_at"
-    t.index ["provider", "uid"], name: "index_authentications_on_provider_and_uid"
-  end
+ActiveRecord::Schema.define(version: 20160516032334) do
 
   create_table "posts", force: :cascade do |t|
     t.string   "title"
