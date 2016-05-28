0d806ffb7a258d7d2302ba89591709bae06a25c3
Hirofumi Wakasugi <baenej@gmail.com>
2016-05-29 00:15:18 +0900

    Improve React DOM class implementation

--- app/assets/javascripts/editor.jsx
+++ app/assets/javascripts/editor.jsx
@@ -1,66 +1,49 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-var PostTitle = React.createClass({
-  componentDidMount: function(event){
-    App.post.preview(document.getElementById('post_title').value);
+var PostEditor = React.createClass({
+  getInitialState: function() {
+    return {title: this.props.title, body: this.props.body};
   },
 
-  handleKeyDown: function(event){
-    App.post.preview({ 'title': event.target.value, 'body': document.getElementById('post_body').value });
+  componentDidMount: function(event) {
+    // Workaround: Delay a bit because it will invoked before WebSocket connection is established...
+    setTimeout(() => { App.post.preview({'title': this.state.title, 'body': this.state.body}); }, 100);
   },
 
-  render: function(){
+  handleChange: function(event) {
+    App.post.preview({
+      'title': document.getElementById('post_title').value,
+      'body': document.getElementById('post_body').value
+    });
+
+    this.setState({
+      'title': document.getElementById('post_title').value,
+      'body': document.getElementById('post_body').value
+    });
+  },
+
+  render: function() {
     return (
-      <div className="form-group">
-        <label htmlFor="post_title">Title</label>
-        <input id="post_title" className="form-control" type="text" name="post[title]" onKeyDown={this.handleKeyDown} defaultValue={this.props.title}></input>
+      <div>
+        <div className="form-group">
+          <label htmlFor="post_title">Title</label>
+          <input id="post_title" className="form-control" type="text" name="post[title]" onChange={this.handleChange} defaultValue={this.props.title}></input>
+        </div>
+        <div className="form-group">
+          <label htmlFor="post_body">Body</label>
+          <textarea rows="35" id="post_body" className="form-control" name="post[body]" onChange={this.handleChange} defaultValue={this.props.body}></textarea>
+        </div>
       </div>
-    )
-  }
-});
-
-var PostBody = React.createClass({
-  componentDidMount: function(event){
-    App.post.preview(document.getElementById('post_body').value);
-  },
-
-  handleKeyDown: function(event){
-    App.post.preview({ 'title': document.getElementById('post_title').value, 'body': event.target.value });
-  },
-
-  render: function(){
-    return (
-      <div className="form-group">
-        <label htmlFor="post_body">Body</label>
-        <textarea rows="35" id="post_body" className="form-control" name="post[body]" onKeyDown={this.handleKeyDown} defaultValue={this.props.body}></textarea>
-      </div>
-    )
-  }
-});
-
-var PostPreview = React.createClass({
-  getInitialState: function(){
-    return { text: this.props.body };
-  },
-
-  update: function(val){
-    this.setState({text: val})
-  },
-
-  render: function(){
-    return (
-      <div className="preview" dangerouslySetInnerHTML={{__html: this.state.text}}></div>
     )
   }
 });
 
 var Markdown = React.createClass({
-  render: function(){
+  render: function() {
     return (
       <div>
-        <PostTitle {...this.props}/>
-        <PostBody {...this.props}/>
+        <PostEditor {...this.props}/>
       </div>
     )
   }
@@ -71,13 +54,16 @@ export class Editor {
     this.element = element
       this._props = props
   }
+
   get props () {
     return this._props
   }
+
   set props (props) {
     Object.assign(this._props, props)
       return this.render()
   }
+
   render () {
     return ReactDOM.render(
       <Markdown {...this.props} />, this.element
