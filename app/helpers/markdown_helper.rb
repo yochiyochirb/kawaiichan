82997fca1ddc6d667b15f4e608d7cc4314ab227b
Hirofumi Wakasugi <baenej@gmail.com>
2016-06-02 10:09:35 +0900

    Fix markdown redering causes error with blank body

--- app/helpers/markdown_helper.rb
+++ app/helpers/markdown_helper.rb
@@ -1,5 +1,6 @@
 module MarkdownHelper
   def markdown_to_html(source)
+    return '' if source.blank?
     Kramdown::Document.new(source, input: 'GFM', syntax_highlighter: 'rouge').to_html
   end
 end
