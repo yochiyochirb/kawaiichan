df2c24e48e1f2bffc35e83dc371d7e693745a0bf
Hirofumi Wakasugi <baenej@gmail.com>
2016-06-02 13:38:11 +0900

    Change emojify process returns unescaped html

--- app/helpers/emoji_helper.rb
+++ app/helpers/emoji_helper.rb
@@ -1,6 +1,6 @@
 module EmojiHelper
   def emojify(content)
-    html_escape(content).to_str.gsub(/:([\w+-]+):/) do |match|
+    content.to_str.gsub(/:([\w+-]+):/) do |match|
       if emoji = Emoji.find_by_alias($1)
         %(<img alt="#$1" src="#{image_path("emoji/#{emoji.image_filename}")}" style="vertical-align:middle" width="20" height="20" />)
       else
