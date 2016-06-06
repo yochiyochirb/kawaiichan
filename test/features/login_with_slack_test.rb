7b27678aba5695d664c3bfad8454ac083c7ad921
Hirofumi Wakasugi <baenej@gmail.com>
2016-06-06 16:48:48 +0900

    Change auth provider from GitHub to Slack

--- test/features/login_with_slack_test.rb
+++ test/features/login_with_slack_test.rb
@@ -1,22 +1,22 @@
 require 'test_helper'
 
-feature 'login with GitHub' do
+feature 'login with slack' do
   scenario 'must have login link' do
     visit login_path
 
     within '.login-screen__login-link' do
-      page.must_have_content 'Login with GitHub'
+      page.must_have_content 'Login with Slack'
     end
   end
 
-  scenario 'can login with Github' do
-    stub_github_login_with 'alice'
+  scenario 'can login with slack' do
+    stub_slack_login_with 'alice'
 
     visit login_path
-    click_link 'Login with GitHub'
+    click_link 'Login with Slack'
 
     within '.flash-message__notice' do
-      page.must_have_content 'Logged in with Github'
+      page.must_have_content 'Logged in with Slack'
     end
 
     within '.navigation__list' do
