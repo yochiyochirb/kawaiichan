9b79b88277f94686d5fb82c9292ee45528223bab
Hirofumi Wakasugi <baenej@gmail.com>
2016-06-08 17:43:38 +0900

    Move test helper method to proper location

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -13,20 +13,18 @@ class ActiveSupport::TestCase
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
   fixtures :all
 
-  # Add more helper methods to be used by all tests here...
-end
+  # Stub Slack OAuth login with OmniAuth mock
+  def stub_slack_login_with(nickname)
+    user = User.find_by!(nickname: nickname)
 
-# Stub Slack OAuth login with OmniAuth mock
-def stub_slack_login_with(nickname)
-  user = User.find_by!(nickname: nickname)
+    OmniAuth.config.test_mode = true
 
-  OmniAuth.config.test_mode = true
-
-  OmniAuth.config.mock_auth[:slack] = OmniAuth::AuthHash.new(
-    provider: 'slack',
-    uid: user.uid,
-    info: {
-      nickname: user.nickname
-    }
-  )
+    OmniAuth.config.mock_auth[:slack] = OmniAuth::AuthHash.new(
+      provider: 'slack',
+      uid: user.uid,
+      info: {
+        nickname: user.nickname
+      }
+    )
+  end
 end
