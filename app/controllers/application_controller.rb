517009d29b4ec1cc16cbbe94ece3d2a6768c7752
Yuka Kato <satoimo.to.imo@gmail.com>
2016-08-27 21:12:42 +0900

    Setup to track post

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,7 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   before_action :require_login
+  before_action :set_paper_trail_whodunnit
   helper_method :current_user, :logged_in?
 
   private
