53ef72806fd431346e2d5e43058e352a0d20b86b
Yuka Kato <satoimo.to.imo@gmail.com>
2016-08-29 17:35:59 +0900

    Migrate database and update schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160826085152) do
+ActiveRecord::Schema.define(version: 20160827120218) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
