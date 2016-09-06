b95e8cc95e7d8dd9f03ee98418c3ae7e82771739
Yuka Kato <satoimo.to.imo@gmail.com>
2016-09-06 18:50:57 +0900

    One-shot definition :rocket:

--- db/migrate/20160826085152_create_comments.rb
+++ db/migrate/20160826085152_create_comments.rb
@@ -1,15 +1,11 @@
 class CreateComments < ActiveRecord::Migration[5.0]
   def change
     create_table :comments do |t|
-      t.references :user, index: true, null: false
+      t.references :commented_by, index: true, null: false, foreign_key: { to_table: :users }
       t.integer :post_id, null: false
       t.text :body, null: false
 
       t.timestamps
     end
-
-    rename_column :comments, :user_id, :commented_by_id
-
-    add_foreign_key :comments, :users, column: 'commented_by_id'
   end
 end
